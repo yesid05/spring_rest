bd6e579d43b96ba8bd2e65b025e9205da02774bd
Yesid05 <caicedopacho@hotmail.com>
2026-02-04 22:25:26 -0500

    Personalizando los datos de respuesta

--- src/main/java/co/spring/rest/entity/dto/UserDto.java
+++ src/main/java/co/spring/rest/entity/dto/UserDto.java
@@ -1,5 +1,11 @@
 package co.spring.rest.entity.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,16 +17,23 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UserDto {
 
+    @JsonIgnore
     private long id;
 
     private String name;
 
+    @JsonProperty("last_name")
     private String lastName;
 
+    @JsonProperty("birth_day")
+    //@JsonFormat(pattern = "dd-MM-yyyy")
     private String birthDay;
 
     private String salary;
 
-    private boolean active;    
+    private boolean active;
+
+    @JsonInclude(value = Include.NON_NULL)
+    private String phoneNumber;
 
 }
